79b73368c6eed80e495f49d527031999e8ce5a05
Alan Lira <alan_lirasp@hotmail.com>
2019-07-16 22:04:39 -0300

    Vue parte 2

--- Vue/src/components/conteudo/despesas/FormDespesas.vue
+++ Vue/src/components/conteudo/despesas/FormDespesas.vue
@@ -1,19 +1,40 @@
 <template>
   <section class="form-container" id="form-despesa-container">
     <h4 class="titulo-form">Adicionar despesa</h4>
-    <form id="form-adicionar-despesa" class="form" name="form-despesa" action="#">
+    <form
+      @submit="submit"
+      id="form-adicionar-despesa"
+      class="form"
+      name="form-despesa"
+      action="#">
       <div class="linha-form">
         <label for="descricao">Descrição</label>
-        <input type="text" id="descricao" name="descricao" required />
+        <input
+          type="text"
+          v-model="despesa.descricao"
+          name="descricao"
+          required />
       </div>
       <div class="linha-form">
         <label for="valor">Valor</label>
-        <input type="number" name="valor" id="valor" step="0.01" required />
+        <input
+          type="number"
+          name="valor"
+          v-model="despesa.valor"
+          step="0.01"
+          required />
       </div>
       <div class="linha-form">
         <label for="categoria">Categoria</label>
-        <select name="categoria" id="categoria" required>
+        <select
+          name="categoria"
+          v-model="despesa.categoria"
+          required>
           <option value>Selecione uma categoria</option>
+          <option
+            :key="categoria.identificador"
+            :value="categoria.identificador"
+            v-for="categoria in categorias">{{categoria.descricao}}</option>
         </select>
       </div>
       <div class="linha-form">
@@ -24,7 +45,41 @@
 </template>
 
 <script>
-export default {};
+import configuracoes from '@/services/configuracoes';
+import despesasService from '@/services/despesas';
+
+export default {
+  data() {
+    return {
+      despesa: {
+        descricao: '',
+        valor: 0,
+        categoria: '',
+        data: new Date(new Date().setHours(0,0,0,0)).toISOString()
+      },
+      categorias: configuracoes.categorias
+    }
+  },
+  methods: {
+    async submit(e) {
+      e.preventDefault();
+      try {
+        await despesasService.adicionar(this.despesa);
+
+        this.despesa = {
+          descricao: '',
+          valor: 0,
+          categoria: '',
+          data: new Date(new Date().setHours(0,0,0,0)).toISOString()
+        };
+
+        alert('Despesa salva com sucesso');
+      } catch {
+        alert('Ocorreu um erro ao salvar a despesa');
+      }
+    }
+  }
+};
 </script>
 
 <style>
