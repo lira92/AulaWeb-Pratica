06fe271b89967bca99641f90760e96c2eaa7bc57
Alan Lira <alan_lirasp@hotmail.com>
2019-07-16 23:03:18 -0300

    criado componentes compartilhados

---
+++ Vue/src/components/compartilhado/GrupoTransacoes.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="grupo-container">
+    <tr key="{props.grupo}">
+      <td class="coluna-data" colspan="2">{{dataFormatada}}</td>
+    </tr>
+    <TransacaoGrupo
+      v-for="transacao in transacoes"
+      :key="grupo+transacao.descricao"
+      :transacao="transacao"
+      :obterCategoria="obterCategoria"
+    />
+  </div>
+</template>
+
+<script>
+import formatacoesService from "@/services/formatacoes";
+import TransacaoGrupo from "./TransacaoGrupo";
+
+export default {
+  components: {
+    TransacaoGrupo
+  },
+  props: {
+    grupo: String,
+    transacoes: Array,
+    obterCategoria: Function
+  },
+  computed: {
+    dataFormatada() {
+      return formatacoesService.formatarData(this.grupo);
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+td {
+  padding: 10px 0;
+  border-bottom: 1px solid #ccc;
+  color: #4b4b4d;
+  font-weight: bold;
+}
+
+td.coluna-data {
+  border: 0;
+  padding-bottom: 0;
+}
+</style>
---
+++ Vue/src/components/compartilhado/TransacaoGrupo.vue
@@ -0,0 +1,56 @@
+<template>
+  <tr>
+    <td class="coluna-descricao-transacao">
+      {{transacao.descricao}}
+      <p class="categoria-label">{{categoria}}</p>
+    </td>
+    <td class="coluna-valor-transacao">{{valorFormatado}}</td>
+  </tr>
+</template>
+
+<script>
+import formatacoesService from "@/services/formatacoes";
+
+export default {
+  props: {
+    transacao: Object,
+    obterCategoria: Function
+  },
+  computed: {
+    valorFormatado() {
+      return formatacoesService.formatarMoeda(this.transacao.valor);
+    },
+    categoria() {
+      return this.obterCategoria(this.transacao);
+    }
+  }
+};
+</script>
+
+<style scoped>
+tr {
+  width: 100%;
+  display: table;
+}
+
+td {
+  padding: 10px 0;
+  border-bottom: 1px solid #ccc;
+  color: #4b4b4d;
+  font-weight: bold;
+}
+
+td.coluna-descricao-transacao {
+  padding-left: 30px;
+}
+
+td.coluna-descricao-transacao p {
+  font-size: 0.7em;
+  opacity: 0.8;
+  margin-top: 3px;
+}
+
+td.coluna-valor-transacao {
+  text-align: right;
+}
+</style>
