cb9a5c47781ad9ab0dfdbbf62751e55394ebce77
Alan Lira <alan_lirasp@hotmail.com>
2019-08-15 22:39:09 -0300

    Alterações para deploy

--- JavaScript/webpack.config.js
+++ JavaScript/webpack.config.js
@@ -1,5 +1,7 @@
 const CopyPlugin = require('copy-webpack-plugin');
 const WorkboxPlugin = require('workbox-webpack-plugin');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'production',
@@ -15,12 +17,19 @@ module.exports = {
             { from: 'node_modules/@fortawesome/fontawesome-free/webfonts', to: 'webfonts' },
             { from: 'node_modules/xlnt/dist/xlnt.wasm', to: 'xlnt.wasm' },
         ]),
-        new WorkboxPlugin.GenerateSW()
+        new WorkboxPlugin.GenerateSW(),
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        })
     ],
     devServer: {
         disableHostCheck: true
     },
     node: {
         fs: "empty"
+    },
+    output: {
+        path: path.resolve(__dirname, "../backendFunctions/public"),
+        filename: "[name].[chunkhash].js"
     }
 }
